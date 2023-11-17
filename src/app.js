6b4db9afa8c1b826aae57084331f9860411b2553
valentin di domenica <valendido69@gmail.com>
2023-11-17 20:36:23 -0300

    add url front

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const dotenv=require('dotenv')
+dotenv.config()
+const URL_FRONT=process.env.FRONTEND_URL
 
 require('./db.js');
 
@@ -15,7 +18,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', URL_FRONT); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
